a5c1a231cf9125968f793912747988adbe049c36
AurinyАртём <gudilovart@gmail.com>
2022-09-01 11:26:32 +0600

    dev0.0.3

--- src/main/java/net/auriny/cosrpg/CoSRPGClient.java
+++ src/main/java/net/auriny/cosrpg/CoSRPGClient.java
@@ -1,13 +1,10 @@
 package net.auriny.cosrpg;
 
-import net.auriny.cosrpg.networking.CosMessages;
 import net.fabricmc.api.ClientModInitializer;
 
 public class CoSRPGClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         System.out.println();
-
-        CosMessages.registerS2CPackets();
     }
 }
